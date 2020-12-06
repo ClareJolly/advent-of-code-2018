4ae34e8358ad2495db463d192bb0fefbe795d596
Clare Jolly <clajollyuk@gmail.com>
2020-12-06 23:18:39 +0000

    day 2 part 2 done

--- src/02/index.test.js
+++ src/02/index.test.js
@@ -3,6 +3,6 @@ import run from '.';
 describe('Run all', () => {
   it('returns the expected values', () => {
     const result = run();
-    expect(result).toEqual({ part1: 5928, part2: undefined });
+    expect(result).toEqual({ part1: 5928, part2: 'bqlporuexkwzyabnmgjqctvfs' });
   });
 });
--- src/02/part2/index.js
+++ src/02/part2/index.js
@@ -1,3 +1,34 @@
-const part2 = (inputData) => {};
+const part2 = (inputData) => {
+  const remap = inputData.map((item) => item.split(''));
+  const close = [];
+  remap.forEach((item) => {
+    const closeMatch = [];
+    remap.forEach((itm) => {
+      let match = 0;
+      let nonMatches = [];
+      for (let i = 0; i < itm.length; i++) {
+        if (itm[i] === item[i]) {
+          match++;
+        } else {
+          nonMatches.push({
+            itemPos: i,
+            // itemDiff: item[i],
+            // item: item.join(''),
+            // itmPos: i,
+            // itmDiff: itm[i],
+            // itm: itm.join(''),
+          });
+        }
+      }
+
+      if (match === item.length - 1) {
+        delete item[nonMatches[0].itemPos];
+        closeMatch.push(item.join(''));
+      }
+    });
+    if (closeMatch.length) close.push(...closeMatch);
+  });
+  return close[0];
+};
 
 export default part2;
